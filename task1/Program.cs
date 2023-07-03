4aa88e89d09079a739c1f14f9f7c9cf45f160bc2
«Max» <maks28isaev@gmail.com>
2023-07-03 15:48:54 +0300

    dz

---
+++ task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+System.Console.Write("Введите количество чисел: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[i];
+FillArray(array);
+int count = 0;
+Zero(array);
+
+void FillArray(int[] array)
+{
+    for (int z = 0; z < array.Length; z++)
+    {
+        System.Console.Write("Введите чисело: ");
+        array[z] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void Zero(int[] array)
+{
+    for (int x = 0; x < array.Length; x++)
+    {
+      if(array[x]>0)
+      {
+        count++;
+      }
+    }
+   System.Console.WriteLine(count);
+}
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("Введите коэффициэнты:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double y = 0;
+double x = 0;
+
+x = ((b2 - b1) / k1) / (1 - k2 / k1);
+y = k2 * x + b2;
+
+System.Console.WriteLine(x);
+System.Console.WriteLine(y);
+
